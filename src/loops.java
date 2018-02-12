5ad9fdf07bd31be517a46e2b2462a9827470e5bc
Derian123 <derian.escobar@yahoo.com>
2018-02-12 14:41:36 -0500

    Loops

---
+++ src/loops.java
@@ -0,0 +1,16 @@
+/**
+ * Created by derianescobar on 2/12/18.
+ */
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+    }
+}
